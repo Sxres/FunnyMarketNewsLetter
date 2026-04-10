03d0e7c262e1a00fdbadbdb7df96078086adad29
Passdaboof <sorescudragos847@gmail.com>
2026-04-10 00:15:57 -0400

    small font change

--- svelte-frontend/src/routes/+layout.svelte
+++ svelte-frontend/src/routes/+layout.svelte
@@ -24,8 +24,7 @@
 		height: 100%;
 		background: #000;
 		color: #eaeaea;
-		font-family: 'Overused Grotesk', -apple-system, BlinkMacSystemFont, 'Segoe UI', system-ui,
-			sans-serif;
+		font-family: 'Overused Grotesk', 'Segoe UI', system-ui, sans-serif;
 		font-size: 14px;
 		-webkit-font-smoothing: antialiased;
 	}
